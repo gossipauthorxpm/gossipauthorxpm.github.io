090a4712aa0c85e9c6a9b35c59c06cdf27ffe29c
gossipauthorxpm <swyte4@gmaiil.com>
2023-05-19 01:01:02 +0300

    рефактор анимаций и кода

---
+++ src/components/animated-elements/VerticalSlideContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "../elements/Container";
+import {ISlideContainerProps} from "../../types/animated-container";
+import VerticalSlide from "../animates/VerticalSlide";
+
+export default function VerticalSlideContainer(props: ISlideContainerProps): React.JSX.Element {
+    return <>
+        <Container className={props.className}>
+            <VerticalSlide power={props.power}>
+                {props.children}
+            </VerticalSlide>
+        </Container>
+    </>
+}
